eeffda6dcc2541cd5f16ad6e3c047e11bea98298
engray <32039289+engray@users.noreply.github.com>
2018-02-21 13:31:38 +0100

    initial commit

---
+++ subpixel-fix.js
@@ -0,0 +1,19 @@
+//https://stackoverflow.com/questions/42267189/how-to-get-value-translatex-by-javascript
+function getTranslateX(current_el) {
+    var style = window.getComputedStyle(current_el);
+    var matrix = new WebKitCSSMatrix(style.webkitTransform);
+    return matrix.m41;
+}
+
+//subpixel fix
+function subpixelFix() {
+    var subpixelElement = document.querySelectorAll(".subpixel-fix");
+    subpixelElement.forEach(function(current_el) {
+        if (getTranslateX(current_el) % 2) {
+            console.log(current_el);
+            var tempValue = getTranslateX(current_el);
+            tempValue.toFixed(0);
+            current_el.style.transform = "translate(" + tempValue.toFixed(0) + "px,0)";
+        }
+    });
+}
