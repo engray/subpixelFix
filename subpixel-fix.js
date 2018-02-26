85c0a2c43adf341ef415a91d62cf3bc642178faf
Maciej Salamon <32039289+engray@users.noreply.github.com>
2018-02-26 16:48:28 +0100

    added translateY handling

--- subpixel-fix.js
+++ subpixel-fix.js
@@ -1,19 +1,24 @@
 //https://stackoverflow.com/questions/42267189/how-to-get-value-translatex-by-javascript
-function getTranslateX(current_el) {
-    var style = window.getComputedStyle(current_el);
+function getTranslateX(cur_el) {
+    var style = window.getComputedStyle(cur_el);
     var matrix = new WebKitCSSMatrix(style.webkitTransform);
     return matrix.m41;
+}
+
+function getTranslateY(cur_el) {
+    var style = window.getComputedStyle(cur_el);
+    var matrix = new WebKitCSSMatrix(style.webkitTransform);
+    return matrix.m42;
 }
 
 //subpixel fix
 function subpixelFix() {
     var subpixelElement = document.querySelectorAll(".subpixel-fix");
-    subpixelElement.forEach(function(current_el) {
-        if (getTranslateX(current_el) % 2) {
-            console.log(current_el);
-            var tempValue = getTranslateX(current_el);
-            tempValue.toFixed(0);
-            current_el.style.transform = "translate(" + tempValue.toFixed(0) + "px,0)";
+    subpixelElement.forEach(function(cur_el) {
+        if ((getTranslateX(cur_el) % 2) || (getTranslateY(cur_el) % 2)) {
+            var tempValueX = getTranslateX(cur_el);
+            var tempValueY = getTranslateY(cur_el);
+            cur_el.style.transform = "translate(" + tempValueX.toFixed(0) + "px," + tempValueY.toFixed(0) + "px)";
         }
     });
 }
